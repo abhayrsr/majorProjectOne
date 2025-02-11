4e3c186c802032ef916af1adada67a113b52cd38
abhay <abhayrajsinghrana123@gmail.com>
2025-02-11 17:36:48 +0530

    feat: created wishlist schema

---
+++ backend/src/routes/wishListApi.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const wishListRouter = express.Router();
+const wishlistClothes = require("../models/wishlist.model");
+
+wishListRouter.post("/", async(req, res) => {
+    
+})
